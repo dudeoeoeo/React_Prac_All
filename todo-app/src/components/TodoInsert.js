f12c9f813ec11395464ef4d183c49c01b6d82d1c
dudeoeoeo <dudekekek@naver.com>
2021-11-04 20:34:47 +0900

    통합 테스트

---
+++ todo-app/src/components/TodoInsert.js
@@ -0,0 +1,26 @@
+import React, { useCallback, useState } from 'react';
+import '.TodoInsert.scss';
+import { MdAdd } from 'react-icons/md';
+
+const TodoInsert = () => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  return (
+    <form className="TodoInsert">
+      <input
+        placeholder="할 일을 입력하세요."
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
