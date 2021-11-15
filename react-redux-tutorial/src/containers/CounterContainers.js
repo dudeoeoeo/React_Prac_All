9edfc7cd3557df1088dfc7e0ae91898fa044f8a2
dudeoeoeo <dudekekek@naver.com>
2021-11-15 19:04:43 +0900

    react-redux

--- react-redux-tutorial/src/containers/CounterContainers.js
+++ react-redux-tutorial/src/containers/CounterContainers.js
@@ -1,28 +1,44 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainers = ({number, increase, decrease}) => {
+// const CounterContainers = ({number, increase, decrease}) => {
+//     return (
+//         <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+//     );
+// };
+
+const CounterContainers = () => {
+    const number = useSelector(state => (
+        state.counter.number
+    ));
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+        <Counter number={number} 
+            onIncrease={onIncrease}
+            onDecrease={onDecrease}
+        />
     );
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
+export default CounterContainers;
+// const mapStateToProps = state => ({
+//     number: state.counter.number,
+// });
 
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease: () => {
-        dispatch(decrease());
-    },
-});
+// const mapDispatchToProps = dispatch => ({
+//     increase: () => {
+//         dispatch(increase());
+//     },
+//     decrease: () => {
+//         dispatch(decrease());
+//     },
+// });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(CounterContainers);
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps,
+// )(CounterContainers);
