8480b0da40e2aa422aebdded89a9e70df55465fa
dudeoeoeo <dudekekek@naver.com>
2021-11-19 09:09:25 +0900

    react-middleware finished

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -9,10 +9,13 @@ import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import createSagaMiddleware from '@redux-saga/core';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk, sagaMiddleware));
+const store = createStore(
+  rootReducer, 
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,23 +1,77 @@
-import { handleActions } from "redux-actions";
+import { call, put, takeLatest } from "@redux-saga/core/effects";
+import { createAction, handleActions } from "redux-actions";
 import * as api from '../lib/api';
+import createRequestSaga from "../lib/createRequestSaga";
 import createRequestThunk from "../lib/createRequestThunk";
+import { finishLoading, startLoading } from "./loading";
 
 // 액션 타입을 선언한다.
 // 한 요청당 세 개를 만들어야 한다.
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-// const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-// const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수를 생성한다.
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치한다.
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, id => id);
+export const getUsers = createAction(GET_USERS);
+
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
+
+// function* getPostSaga(action) {
+//     yield put(startLoading(GET_POST)); // 로딩 시작
+//     // 파라미터로 action을 받아 오면 액션의 정보를 조회할 수 있다.
+//     try {
+//         // call을 사용하면 Promise를 반환하는 함수를 호출하고, 기다릴 수 있다.
+//         // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수이다.
+//         const post = yield call(api.getPost, action.payload); // api.getPost(action.payload) 를 의미
+//         yield put({
+//             type: GET_POST_SUCCESS,
+//             payload: post.data
+//         });
+//     } catch (e) {
+//         // try/catch 문을 사용하여 에러도 잡을 수 있다.
+//         yield put({
+//             type: GET_POST_FAILURE,
+//             payload: e,
+//             error: true
+//         });
+//     }
+//     yield put(finishLoading(GET_POST));
+// }
+
+// function* getUsersSaga() {
+//     yield put(startLoading(GET_USERS));
+//     try {
+//         const users = yield call(api.getUsers);
+//         yield put({
+//             type: GET_USERS_SUCCESS,
+//             payload: users.data
+//         });
+//     } catch (e) {
+//         yield put({
+//             type: GET_USERS_FAILURE,
+//             payload: e,
+//             error: true
+//         });
+//     }
+//     yield put(finishLoading(GET_USERS));
+// }
+
+export function* sampleSaga() {
+    yield takeLatest(GET_POST, getPostSaga);
+    yield takeLatest(GET_USERS, getUsersSaga);
+}
+
+// export const getPost = createRequestThunk(GET_POST, api.getPost);
+// export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // export const getPost = id => async dispatch => {
 //     dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
