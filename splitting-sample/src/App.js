fdd165c4a47d7f12fd20cd09cdcc32f7d64bb8c6
dudeoeoeo <dudekekek@naver.com>
2021-11-21 18:41:04 +0900

    react code-splitting

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import notify from './notify';
+import React, { Suspense, useState } from 'react';
+import loadable from '@loadable/component';
+
+// const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 function App() {
+  // const onClick = () => {
+  //   // import를 함수로 사용하면 Promise를 반환한다. 이렇게 import를 함수로 사용하는 문법은
+  //   // 비록 아직 표준 자바스크립트가 아니지만, state-3 단계에 있는 dynamic import 문법이다.
+  //   import('./notify').then(result => result.default());
+  // }
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <p onClick={notify}> Hello React! </p>
+          <p onClick={onClick} onMouseOver={onMouseOver}> Hello React! </p>
+            {visible && <SplitMe />}
       </header>
     </div>
   );
