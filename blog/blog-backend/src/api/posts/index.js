3267ac9e970b94f6fb5d8d75c6fc14aa17e7dc96
dudeoeoeo <dudekekek@naver.com>
2021-12-08 10:38:31 +0900

    api

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,5 +1,5 @@
 import Router from "koa-router";
-import * as postsCtrl from './posts.ctrl.js-ex';
+import * as postsCtrl from './posts.ctrl.js';
 
 const posts = new Router();
 
@@ -13,9 +13,9 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
+posts.get('/:id', postsCtrl.checkObjectId,postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
 //posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
 
 export default posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,6 +1,36 @@
+import mongoose from 'mongoose';
 import Post from '../../modules/post';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if(!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        return;
+    }
+    return next();
+};
 
 export const write = async ctx => {
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있음을 검증
+        title: Joi.string().required(), // required() 가 있으면 필수 항목
+        body: Joi.string().required(), 
+        tags: Joi.array()
+            .items(Joi.string())
+            .required(), // 문자열로 이루어진 배열
+    });
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
@@ -16,9 +46,32 @@ export const write = async ctx => {
 };
 
 export const list = async ctx => {
+    // query는 문자열이기 때문에 숫자로 변환해 주어야 한다.
+    // 값이 주어지지 않았다면 1을 기본으로 사용한다.
+    const page = parseInt(ctx.query.page || '1', 10);
+
+    if (page < 1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
-        const posts = await Post.find().exec();
-        ctx.body = posts;
+        // const posts = await Post.find().exec();
+        const posts = await Post.find()
+            .sort({ _id: -1 })
+            .limit(10)
+            .skip((page - 1) * 10)
+            .lean()
+            .exec();
+        const postCount = await Post.countDocuments().exec();
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
+        ctx.body = posts
+            // .map(post => post.toJSON())
+            .map(post => ({
+                ...post,
+                body:
+                    post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+            }));
     } catch (e) {
         ctx.throw(500, e);
     }
@@ -50,6 +103,20 @@ export const remove = async ctx => {
 
 export const update = async ctx => {
     const { id } = ctx.params;
+    
+    const schema = Joi.object().keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),
+    });
+
+    // 검증 후 검증 실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if(result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true // 이 값을 설정하면 업데이트된 데이터를 반환한다.
