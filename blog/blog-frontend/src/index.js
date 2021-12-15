3be8debbeb9c42c8aca897727a1eb644be8b3575
dudeoeoeo <dudekekek@naver.com>
2021-12-15 18:19:50 +0900

    auth

--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from '../node_modules/react-router-dom/index';
+import { createStore } from 'redux';
+import rootReducer from './modules/index';
+import { composeWithDevTools } from '../node_modules/redux-devtools-extension/index';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
