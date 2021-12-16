1b898aa74bb450b9b17e3503ad81bb6b99bc4112
dudeoeoeo <dudekekek@naver.com>
2021-12-16 18:42:58 +0900

    reg&login

--- blog/blog-frontend/src/pages/LoginPage.js
+++ blog/blog-frontend/src/pages/LoginPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
 
 const LoginPage = () => {
     return (
         <div>
-            
+            <AuthTemplate>
+                <AuthForm type={'login'} />
+            </AuthTemplate>
         </div>
     );
 };
--- blog/blog-frontend/src/pages/RegisterPage.js
+++ blog/blog-frontend/src/pages/RegisterPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
 
 const RegisterPage = () => {
     return (
         <div>
-            
+            <AuthTemplate>
+                <AuthForm type={'register'} />
+            </AuthTemplate>
         </div>
     );
 };
