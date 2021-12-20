85862b0bb436f0401d8bf623aff5080d938c392b
dudeoeoeo <dudekekek@naver.com>
2021-12-20 18:57:01 +0900

    redux saga

--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -1,8 +1,27 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "../../node_modules/immer/dist/immer";
+import {takeLatest} from 'redux-saga/effects';
+import createRequestSaga, { createRequestActionTypes } from "../lib/createRequestSaga";
+import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+    'auth/REGISTER',
+);
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+    'auth/LOGIN',
+);
+
+// const REGISTER = 'auth/REGISTER';
+// const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+// const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+// const LOGIN = 'auth/LOGIN';
+// const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+// const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 
 export const changeField = createAction(
     CHANGE_FIELD,
@@ -14,6 +33,24 @@ export const changeField = createAction(
 );
 export const initializeForm = createAction(INITIALIZE_FORM, form => form); // register / login
 
+export const register = createAction(REGISTER, ({ username, password }) => ({
+    username,
+    password,
+}));
+
+export const login = createAction(LOGIN, ({ username, password }) => ({
+    username,
+    password,
+}));
+
+// saga 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+    yield takeLatest(REGISTER, registerSaga);
+    yield takeLatest(LOGIN, loginSaga);
+}
+
 const initialState = {
     register: {
         username: '',
@@ -24,6 +61,8 @@ const initialState = {
         username: '',
         password: '',
     },
+    auth: null,
+    authError: null,
 };
 
 const auth = handleActions(
@@ -34,6 +73,26 @@ const auth = handleActions(
         [INITIALIZE_FORM]: (state, {payload: form}) => ({
             ...state,
             [form]: initialState[form],
+            authError: null, // 폼 전환 시 회원 인증 에러 초기화
+        }),
+        // 회원가입 성공
+        [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        [REGISTER_FAILURE]: (state, { payload: error }) => ({
+            ...state,
+            authError: error,
+        }),
+        [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        [LOGIN_FAILURE]: (state, { payload: error }) => ({
+            ...state,
+            authError: error,
         }),
     },
     initialState,
