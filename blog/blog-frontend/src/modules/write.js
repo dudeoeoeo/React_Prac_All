b2ef819fa4085be696eddff3c301c54a7abc69f7
dudeoeoeo <dudekekek@naver.com>
2022-01-08 12:43:28 +0900

    write

--- blog/blog-frontend/src/modules/write.js
+++ blog/blog-frontend/src/modules/write.js
@@ -3,6 +3,7 @@ import createRequestSaga, { createRequestActionTypes } from "../lib/createReques
 import * as postsApi from '../lib/api/posts';
 import { takeLatest } from 'redux-saga/effects';
 
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
 const INITIALIZE = 'write/INITIALIZE'; // 모든 내용 초기화
 const CHANGE_FIELD = 'write/CHANGE_FIELD'; // 특정 key 값 바꾸기
 const [
@@ -10,6 +11,12 @@ const [
     WRITE_POST_SUCCESS,
     WRITE_POST_FAILURE,
 ] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
+
+const [
+    UPDATE_POST,
+    UPDATE_POST_SUCCESS,
+    UPDATE_POST_FAILURE,
+] = createRequestActionTypes('write/UPDATE_POST'); // 포스트 수정
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
@@ -21,11 +28,21 @@ export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
     body,
     tags,
 }));
+export const updatePost = createAction(UPDATE_POST, ({ id, title, body, tags }) => ({
+    id,
+    title,
+    body,
+    tags,
+}));
+
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
 
 // 사가 생성
 const writePostSaga = createRequestSaga(WRITE_POST, postsApi.writePost);
+const updatePostSaga = createRequestSaga(UPDATE_POST, postsApi.updatePost);
 export function* writeSaga() {
     yield takeLatest(WRITE_POST, writePostSaga);
+    yield takeLatest(UPDATE_POST, updatePostSaga);
 }
 
 const initialState = {
@@ -34,6 +51,7 @@ const initialState = {
     tags: [],
     post: null,
     postError: null,
+    originalPostId: null,
 };
 
 const write = handleActions(
@@ -58,6 +76,21 @@ const write = handleActions(
         [WRITE_POST_FAILURE]: (state, { payload: { postError }}) => ({
             ...state,
             postError,
+        }),
+        [UPDATE_POST_SUCCESS]: (state, { payload: post }) => ({
+            ...state,
+            post,
+        }),
+        [UPDATE_POST_FAILURE]: (state, { payload: postError }) => ({
+            ...state,
+            postError,
+        }),
+        [SET_ORIGINAL_POST]: (state, { payload: post }) => ({
+            ...state,
+            title: post.title,
+            body: post.body,
+            tags: post.tags,
+            originalPostId: post._id,
         }),
     },
     initialState,
